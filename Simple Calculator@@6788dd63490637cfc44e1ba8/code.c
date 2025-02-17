5622cad8b89de325649ffa17915727c0fde6efb1
GarishmaChug <garishma3016.beai24@chitkara.edu.in>
2025-02-18 01:31:03 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,7 +2,7 @@
 int main(){
     int n,m;
     scanf("%d %d",&n,&m);
-    printf("%d + %d = %d",n,m,n+m);
+    printf("%d",n,m,n+m);
     //  printf("%d",a-b);
     // printf("%d",a*b);
     // printf("%d",a/b);
