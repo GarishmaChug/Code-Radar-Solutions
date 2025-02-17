19ea662f2cc9e3e93563cc045df7b7580abb4d68
GarishmaChug <garishma3016.beai24@chitkara.edu.in>
2025-02-18 02:01:58 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@ int main(){
     int n,m;
     char operation;
     scanf("%d %d %c",&n,&m,&operation);
-    if(%d %d %c){
-        printf("%d",n+m+operation);
+    if(operation=='+'){
+        printf("%d",n+m);
     }
 }
